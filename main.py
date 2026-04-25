4250169e2ba7f8df06d6223267c714bfd6905353
dannykao0715-code <dannykao0715@gmail.com>
2026-04-25 08:17:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 import requests
 import pandas as pd
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return {"message": "stock ai running"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
 
 def get_data(symbol):
     url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}.TW"
@@ -17,20 +26,36 @@ def get_data(symbol):
 
     return pd.Series(close).dropna(), pd.Series(volume).dropna()
 
+
 def score(price, volume):
     s = 0
 
+    # 底底高
     if price.iloc[-1] > price.iloc[-5]:
-        s += 50
+        s += 30
 
-    if volume.iloc[-1] > volume.mean():
-        s += 50
+    # 爆量
+    if volume.iloc[-1] > volume.mean()*1.5:
+        s += 30
+
+    # 突破前高
+    if price.iloc[-1] > price.max()*0.95:
+        s += 40
 
     return s
 
+
 @app.get("/recommendations")
 def rec():
-    stocks = ["2330", "2317", "2303"]
+    stocks = [
+        "2330",
+        "2317",
+        "2303",
+        "2603",
+        "2454",
+        "2382"
+    ]
+
     result = []
 
     for s in stocks:
@@ -38,7 +63,7 @@ def rec():
             price, volume = get_data(s)
             sc = score(price, volume)
 
-            if sc >= 50:
+            if sc >= 60:
                 result.append({
                     "stock": s,
                     "score": sc
