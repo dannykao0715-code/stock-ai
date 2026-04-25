a9d7e81ac411a9964d0a7c5c35ed21bc72556322
dannykao0715-code <dannykao0715@gmail.com>
2026-04-25 09:35:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,6 @@ def index():
                            otc_idx=otc_idx)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
